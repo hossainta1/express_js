e28051d75a200e3671e630652c907465ff55a1d3
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-06-01 23:20:54 +0600

    express js router

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const todosRouter = express_1.default.Router();
+app.use("/todos", todosRouter);
+todosRouter.get("/all-todos", (req, res) => {
+    const data = fs_1.default.readFileSync(filePath, { encoding: "utf8" });
+    res.json({
+        message: "From todos router",
+        data: JSON.parse(data),
+    });
+});
+const filePath = path_1.default.join(__dirname, "../db/todo.json");
 app.get("/", (req, res) => {
     res.send("Welcome to todos App ");
 });
 app.get("/todos", (req, res) => {
-    res.send("I am learning Express js with type script ");
+    // console.log(req.query);
+    console.log(req.params);
+    const data = fs_1.default.readFileSync(filePath, { encoding: "utf8" });
+    console.log(data);
+    res.json(data);
+});
+app.post("/todos/create-todo", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    res.send("Hellow Prisma");
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,38 @@
 import express, { Application, Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 const app: Application = express();
+app.use(express.json());
+
+const todosRouter = express.Router();
+app.use("/todos", todosRouter);
+
+todosRouter.get("/all-todos", (req: Request, res: Response) => {
+  const data = fs.readFileSync(filePath, { encoding: "utf8" });
+  res.json({
+    message: "From todos router",
+    data: JSON.parse(data),
+  });
+});
+
+const filePath = path.join(__dirname, "../db/todo.json");
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to todos App ");
 });
 
 app.get("/todos", (req: Request, res: Response) => {
-  res.send("I am learning Express js with type script ");
+  // console.log(req.query);
+  console.log(req.params);
+  const data = fs.readFileSync(filePath, { encoding: "utf8" });
+  console.log(data);
+  res.json(data);
+});
+
+app.post("/todos/create-todo", (req: Request, res: Response) => {
+  const data = req.body;
+  console.log(data);
+  res.send("Hellow Prisma");
 });
 
 export default app;
