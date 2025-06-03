bce834622001cdc59daea85fb10bf15c0aef4bf8
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-06-03 20:12:37 +0600

    Add .env file

--- dist/config/mongodb.js
+++ dist/config/mongodb.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.client = void 0;
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://mongodb:mongodb@cluster0.rysigvn.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rysigvn.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0`;
 exports.client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,11 +1,14 @@
+
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://mongodb:mongodb@cluster0.rysigvn.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rysigvn.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0`;
+
 export const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
